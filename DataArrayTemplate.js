7c4b098606747fea43b52d8e54e724cb1e49c00e
Kristopher Takken <68007975+allform-tech@users.noreply.github.com>
2020-08-16 14:05:48 +0930

    Create DataArrayTemplate.js

---
+++ DataArrayTemplate.js
@@ -0,0 +1,76 @@
+var pDatasets = [{
+	
+	pageValues : {
+		pTitle : " ",
+		pUpdated : " ",
+		pContacts : {
+			cInclude : true,
+			cInfo : [
+				[" "],
+				[" "]
+			],
+		},
+		pLinks : {
+			lInclude : true,
+			lInfo : [
+				[" "],
+				[" "]
+			],
+		},
+		pHeaderLinks : {
+			hInclude : true,
+			hInfo : [
+				[" "],
+				[" "]
+			],
+		},
+		//######################
+		//Add more page options
+	},
+	
+	datasets : [
+		{
+			dOptions : {
+				dTitle : " ",
+				dType : " ",
+				dMap : true,
+				dFinance : true,
+				dAssignedTo : true,
+				dDue : 30,
+				dFY : true,
+			},
+		
+			dCharts : {
+				dChartOptions1 : {
+					cLegend : true,
+					cToolTip : true,
+					cTitle : true,
+					dChartTitles : [" ",],
+					dChartType : [" ",],
+					dChartToolTip : [" ",],
+					dChartLegend : [" ",],
+					cColors : [" ",],
+					cLayers : 1,
+				},
+				//#####################
+				//Add more charts
+			},
+		
+			dTable : [
+				dHeaders : [" ",],
+				dValues : [
+					[" ",],
+					[" ",],
+					[" ",]
+				],
+			}
+		},//End dataset 1
+		{
+		
+			//######################
+			//More datasets here
+			
+		},//End dataset 2
+	],
+	
+}]
