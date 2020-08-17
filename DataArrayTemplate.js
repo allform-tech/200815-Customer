513ed54caee9ee320449d1e2733ffad22ca9920d
Kristopher Takken <68007975+allform-tech@users.noreply.github.com>
2020-08-17 21:07:21 +0930

    Update DataArrayTemplate.js

--- DataArrayTemplate.js
+++ DataArrayTemplate.js
@@ -1,76 +1,60 @@
-var pDatasets = [{
-	
-	pageValues : {
-		pTitle : " ",
-		pUpdated : " ",
-		pContacts : {
-			cInclude : true,
-			cInfo : [
-				[" "],
-				[" "]
-			],
-		},
-		pLinks : {
-			lInclude : true,
-			lInfo : [
-				[" "],
-				[" "]
-			],
-		},
-		pHeaderLinks : {
-			hInclude : true,
-			hInfo : [
-				[" "],
-				[" "]
-			],
-		},
-		//######################
-		//Add more page options
+datasets = [{
+
+pageOptions : {
+	pageTitle : "My Page",
+	backgroundImage : "url",
+	backgroundColor : #fff,
+	primaryColor : #000,
+	secondaryColor : red,
+	tersiaryColor : gray,
+	primaryTextColor : #fff,
+	secondaryTextColor : red,
+},
+
+menuBarLinks : [
+	["Title", "url"],
+	["Title", "url"],
+	...
+],
+
+datasets : [
+	{
+		datasetTitle : "Tasks",
+		datasetName : "Tasks",
+		linkedTimeline : true,
+		linkedColumns : [1,2,3,4],
+		primaryDataset : true,
+		masterDashboard : true,
+		ownerDashboards : 3,
+		locationDashboard : 4,
+		metrics : [
+			[3, "sum"],
+			[4, "average"],
+			...
+		],
+		timeline : true,
+		map : true,
+		dashboard : [0,1,5,10],
+		charts : ["line", "pie",...],
+		grid : 1,
+		headers : [
+			"Id",
+			"Title",
+			...
+		],
+		datasetBody : [
+			[1, "title 1", ...],
+			[1, "title 2", ...],
+			...
+		]
 	},
-	
-	datasets : [
-		{
-			dOptions : {
-				dTitle : " ",
-				dType : " ",
-				dMap : true,
-				dFinance : true,
-				dAssignedTo : true,
-				dDue : 30,
-				dFY : true,
-			},
-		
-			dCharts : {
-				dChartOptions1 : {
-					cLegend : true,
-					cToolTip : true,
-					cTitle : true,
-					dChartTitles : [" ",],
-					dChartType : [" ",],
-					dChartToolTip : [" ",],
-					dChartLegend : [" ",],
-					cColors : [" ",],
-					cLayers : 1,
-				},
-				//#####################
-				//Add more charts
-			},
-		
-			dTable : [
-				dHeaders : [" ",],
-				dValues : [
-					[" ",],
-					[" ",],
-					[" ",]
-				],
-			}
-		},//End dataset 1
-		{
-		
-			//######################
-			//More datasets here
-			
-		},//End dataset 2
-	],
-	
+	{
+		...
+	},
+	{
+		...
+	},
+]
+]}
+
 }]
