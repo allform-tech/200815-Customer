981bce94aa4ff44a5b9ce01db8e6fc76910794ca
Kristopher Takken <68007975+allform-tech@users.noreply.github.com>
2020-08-17 21:24:15 +0930

    Update DataArrayTemplate.js

--- DataArrayTemplate.js
+++ DataArrayTemplate.js
@@ -17,6 +17,22 @@ menuBarLinks : [
 	...
 ],
 
+mainDashboard : {
+        include : true,
+        title : "My Dashboard",
+        metrics : [
+               [3, "sum", red, "prefix", "Sufix"],
+               [4, "average", blue, "prefix", "Sufix"],
+               ...
+        ],
+        charts : [
+               ["line", 1, 2, "title", "months", [0,1,2]],
+               ["pie", 1, "title", "prefix", "sufix", [0,1,2]],
+               ["map", [1, 2, 3], [0,1,2]],
+               ...
+        ]
+}
+
 datasets : [
 	{
 		datasetTitle : "Tasks",
@@ -28,14 +44,16 @@ datasets : [
 		ownerDashboards : 3,
 		locationDashboard : 4,
 		metrics : [
-			[3, "sum", red],
-			[4, "average", blue],
+			[3, "sum", red, "prefix", "Sufix"],
+                        [4, "average", blue, "prefix", "Sufix"],
 			...
 		],
-		timeline : true,
-		map : true,
-		dashboard : [0,1,5,10],
-		charts : ["line", "pie",...],
+		charts : [
+                        ["line", 1, 2, "title", "months"],
+                        ["pie", 1, "title", "prefix", "sufix"],
+                        ["map", [1, 2, 3]], 
+                        ...
+                ],
 		grid : 1,
 		headers : [
 			"Id",
